542d9809f8fc5905f14b67c1ee31fc8ffd7a70cc
Natalia Ramirez <ntlc.ramirez@gmail.com>
2020-09-13 22:47:00 +1200

    footer and jquery

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@ console.log('js');
 
 $('#drop-nav').hide();
 
+$('#para01').hide();
+
 $(document).ready(function(){
 
 $('.fa-bars').click(function(){
   $('#drop-nav').toggle();
 });
+
+$('#read-more-btn').click(function(){
+    $('#para01').toggle();
+  });
+
 });
