bb7abd343277dc61028def3b19756503e3c24468
Natalia <ntlc.ramirez@gmail.com>
2020-09-14 12:38:43 +1200

    jquery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 console.log('js');
 
 $('#drop-nav').hide();
+var paras = document.getElementsByClassName('para');
+console.log(paras);
+for (var i = 0; i < paras.length; i ++) {
+    paras[i].style.display = 'none';
+}
 
-$('.para').hide();
+// $('.para').hide();
 
 $(document).ready(function(){
 
@@ -20,5 +25,48 @@ var myIndex = $('.read-more-btn').index(this);
 $('.read-less-btn').click(function(){
     $('.para').hide();
   });
+
+
+
+
+// copied code
+
+  $('.read-more-btn').on('click', function(){
+
+      //item name from id of button being clicked;
+      //this refers to what is being clicked
+
+    //  console.log(this.id);
+      // $('#items').append ('<br>' + this.id)
+    //  var itemName = this.id; //storing the value to a variable
+    //  $('#items').append ('<br>' + itemName);
+
+      // console.log($('.menu-button').index(this));
+      var myIndex = $('.read-more-btn').index(this);
+      console.log(myIndex);
+      var sample = document.getElementsByClassName('para')[myIndex];
+      console.log(sample);
+      // var paras = document.getElementsByClassName('para');
+      // console.log(paras);
+      // console.log(paras.length);
+
+      //searching or matching
+  //     for (var i=0; i< paras.length;i++){
+  //       // console.log(itemPrice[i].innerHTML);
+  //       if (myIndex == i){
+  //         // console.log('yes');//
+  //         document.getElementsByClassName('para')[myIndex].style.display ="block";
+  //       } //if ends
+  // } //for loop ends
+
+  }); //.menu-button
+
+
+
+
+
+
+
+
 
   });
