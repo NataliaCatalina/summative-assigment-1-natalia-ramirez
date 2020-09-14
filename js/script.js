ea2ab10ea821142372476f101a5bd6d09541ba36
Natalia Ramirez <ntlc.ramirez@gmail.com>
2020-09-14 22:17:24 +1200

    responsive v0.2

--- js/script.js
+++ js/script.js
@@ -4,41 +4,24 @@ console.log('js');
 
 $('#drop-nav').hide();
 
+// $('#empanadas-btn').click(funtion(){
+//   $('empanadas').show();
+// });
+
+
 var paras = document.getElementsByClassName('para');
 console.log(paras);
 for (var i = 0; i < paras.length; i ++) {
     paras[i].style.display = 'none';
 }
 
-
-
-// $('.para').hide();
-
-
-
 $(document).ready(function(){
-
-
 
 $('.fa-bars').click(function(){
   $('#drop-nav').toggle();
 });
 
-
-
-
-// $('.read-less-btn').click(function(){
-//     $('.para').hide();
-//   });
-
-
-
-
-// copied code
-
-
-
-  $('.read-more-btn').on('click', function(){
+$('.read-more-btn').on('click', function(){
 
       var myIndex = $('.read-more-btn').index(this);
       console.log(myIndex);
@@ -49,7 +32,7 @@ $('.fa-bars').click(function(){
 
   }); //.menu-button
 
-  $('.read-less-btn').on('click', function(){
+$('.read-less-btn').on('click', function(){
 
       var myIndex = $('.read-less-btn').index(this);
       console.log(myIndex);
@@ -59,6 +42,7 @@ $('.fa-bars').click(function(){
      sample.style.display = 'none';
 
   });
+
 
 
   });
