7e0139d819693026c95a385a92d3c8348a297044
Natalia <ntlc.ramirez@gmail.com>
2020-09-15 15:52:36 +1200

    my cart v.2

--- js/script.js
+++ js/script.js
@@ -8,6 +8,55 @@ $('#drop-nav, .read-less-btn').hide();
 //   $('empanadas').show();
 // });
 
+//****************
+
+
+var total =0; //initializing the numeric variable
+
+  $('.menu-button').on('click', function(){
+
+
+    //item name from id of button being clicked;
+    //this refers to what is being clicked
+
+    console.log(this.id);
+    // $('#items').append ('<br>' + this.id)
+    var itemName = this.id; //storing the value to a variable
+    $('#items').append ('<br>' + itemName);
+
+    // console.log($('.menu-button').index(this));
+    var myIndex = $('.menu-button').index(this);
+    console.log(myIndex);
+
+    var itemPrice = document.getElementsByClassName('itemPrice');
+    console.log(itemPrice);
+    console.log(itemPrice.length);
+
+    //searching or matching
+    for (var i=0; i< itemPrice.length;i++){
+      // console.log(itemPrice[i].innerHTML);
+      if (myIndex == i){
+        // console.log('yes');//
+        console.log(itemPrice[i].innerHTML);
+        var price = itemPrice[i].innerHTML;
+        $('#price').append('<br>' + price);
+        total += parseFloat(price);
+        console.log(total);
+      } //if ends
+} //for loop ends
+
+}); //.menu-button
+$('#cartIcon').click(function(){
+  console.log('clicked');
+$('#cart').show();
+$('#menu').hide();
+});
+
+$('#calculate').on('click', function(){
+  document.getElementById('amount').innerHTML = '$' + total.toFixed(2);
+});
+
+//***********************
 
 var paras = document.getElementsByClassName('para');
 console.log(paras);
@@ -56,4 +105,4 @@ $('.read-less-btn').on('click', function(){
 
 
 
-  });
+}); //document.ready() ends
